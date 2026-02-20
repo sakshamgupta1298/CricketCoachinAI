c1ae9adc9f764924282f0ad89a4446ee502c32b4
Saksham Gupta <saksham@Sakshams-MacBook-Air.local>
2026-02-20 21:32:28 +0530

    new server fix

--- backend_script.py
+++ backend_script.py
@@ -8,7 +8,7 @@ import numpy as np
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
-from google import genai
+import google.generativeai as genai
 import csv
 from torchvision import transforms as T
 from pytorchvideo.models.hub import slowfast_r50
